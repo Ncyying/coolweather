d18db1cf4f580ef5327f9963f1e46b5933cd5980
Ncyy <ncyy@gmail.com>
2017-07-25 10:22:20 +0800

    First commit.

---
+++ app/src/main/java/com/example/nyy/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nyy.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
